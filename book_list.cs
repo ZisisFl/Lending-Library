9f40c32f229207fd12762f888b6fc8a30689b22c
Unknown <p15148@students.cs.unipi.gr>
2017-12-13 00:35:16 +0200

    Working State

---
+++ book_list.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lending_Library
+{
+    public partial class book_list : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=" + Application.StartupPath + "\\library.mdf;Integrated Security = True");
+        public book_list()
+        {
+            InitializeComponent();
+        }
+
+        private void book_list_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from [Table] order by book_title ASC";
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    ListViewItem item = new ListViewItem(dr["bookID"].ToString());
+                    item.SubItems.Add(dr["book_title"].ToString());
+                    item.SubItems.Add(dr["book_author"].ToString());
+                    item.SubItems.Add(dr["book_category"].ToString());
+                    item.SubItems.Add(dr["available"].ToString());
+                    item.SubItems.Add(dr["name"].ToString());
+                    item.SubItems.Add(dr["surname"].ToString());
+                    item.SubItems.Add(dr["telephone"].ToString());
+                    item.SubItems.Add(dr["date_lend"].ToString());
+                    item.SubItems.Add(dr["date_return"].ToString());
+
+                    booklist.Items.Add(item);
+                }
+                conn.Close();
+            }
+
+            catch (Exception ex)
+            {
+                conn.Close();
+                MessageBox.Show("ERROR: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ new_book.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Lending_Library
+{
+    partial class new_book
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(new_book));
+            this.label1 = new System.Windows.Forms.Label();
+            this.book_title = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.book_author = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.book_category = new System.Windows.Forms.TextBox();
+            this.add_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Τίτλος:";
+            // 
+            // book_title
+            // 
+            this.book_title.Location = new System.Drawing.Point(15, 46);
+            this.book_title.Name = "book_title";
+            this.book_title.Size = new System.Drawing.Size(100, 20);
+            this.book_title.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Συγγραφέας:";
+            // 
+            // book_author
+            // 
+            this.book_author.Location = new System.Drawing.Point(15, 99);
+            this.book_author.Name = "book_author";
+            this.book_author.Size = new System.Drawing.Size(100, 20);
+            this.book_author.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Κατηγορία:";
+            // 
+            // book_category
+            // 
+            this.book_category.Location = new System.Drawing.Point(139, 46);
+            this.book_category.Name = "book_category";
+            this.book_category.Size = new System.Drawing.Size(100, 20);
+            this.book_category.TabIndex = 15;
+            // 
+            // add_button
+            // 
+            this.add_button.Location = new System.Drawing.Point(15, 166);
+            this.add_button.Name = "add_button";
+            this.add_button.Size = new System.Drawing.Size(86, 30);
+            this.add_button.TabIndex = 16;
+            this.add_button.Text = "Add";
+            this.add_button.UseVisualStyleBackColor = true;
+            this.add_button.Click += new System.EventHandler(this.add_button_Click);
+            // 
+            // new_book
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 214);
+            this.Controls.Add(this.add_button);
+            this.Controls.Add(this.book_category);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.book_author);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.book_title);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "new_book";
+            this.Text = "Add Book";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox book_title;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox book_author;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox book_category;
+        private System.Windows.Forms.Button add_button;
+    }
+}
